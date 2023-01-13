c94e34dab6b0fb085df5d59b74411bcb79891d82
J2DCB <47268745+Junhyeok2004Dec@users.noreply.github.com>
2023-01-14 06:49:52 +0900

    asm
    변경1

--- boot.asm
+++ boot.asm
@@ -1,29 +1,44 @@
-%include "io64.inc"	
-
 section .data
 
 message: db "calculate", 0xa
 len equ $-message
 
 
-getCount: null 
 exportadd: db $-getlen
 
 section .bss
 
 	push rbp
 	mov rbp, rsp 
-	sub rsp, 0E4h
+	sub rsp, 0x0e4h
 
-	mov
-
+	mov rsp, esp
+	sub rsp, 0x0080
+	push rdx
+	int 0x0080
+	
+	mov rdx, rsp
+	
+	sub esp, $-rsp
+	sub rsp, $-rdx
+	int 0x0080
+	
+	str ecx, message
+	mov rex, len
+	push rex
+	int 0x0080
+	
+	push rdx
+	
+	mov esp, rdx
+	push rdx
 
 section .text
+	global _start
+	
+_start:
 
-push rax
-
-global MAIN
-
-CMAIN:
+	mov edx, len
+	mov ecx, message
 
 	
